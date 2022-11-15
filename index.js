d41e561ea0b09310af2fcb1a9590fb06e6f3a2fc
saikrishna4545 <katterasaikrishna@gmail.com>
2022-11-15 10:40:24 +0000

    super over

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <img
+      className="image"
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+    />
+    <img
+      className="image"
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
